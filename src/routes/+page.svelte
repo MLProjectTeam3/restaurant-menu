bb52adb28104ecf7f58f2b2c4f3659fab19b8ecc
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-03-10 23:03:33 +0530

    Set up shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+</script>
+
 <h1 class="text-3xl font-bold text-blue-900">Welcome to our ML team</h1>
+<Button> Click me </Button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
