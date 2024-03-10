f6a696d4382812e451dd587b82470d3058ab8390
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-03-10 22:56:27 +0530

    Set up tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to our ML team</h1>
+<h1 class="text-3xl font-bold text-blue-900">Welcome to our ML team</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
