6a3985185b75081543ea01ee132d1c95554fd596
nidhish654 <nidhish.shettigar.06@gmail.com>
2024-03-10 23:32:33 +0530

    "added link to the button"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <h1 class="text-3xl font-bold text-blue-900">Welcome to our ML team</h1>
-<Button> Click me </Button>
+<Button href = "https://monkeytype.com/"> Click me </Button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
