7ead327172b5816c8f00aa1736a63a0d03178836
nidhish654 <nidhish.shettigar.06@gmail.com>
2024-03-24 00:43:13 +0530

    "Navbar_trial"

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,3 +1,31 @@
-<div class="h-12 p-2 w-full bg-background border-b-2 border-primary border-opacity-15">
-    <p class="text-red-600 font-bold">This is the navbar. Edit your code here.</p>
+<script>
+    import logoImage from "../../assets/icons/logo.png"
+    import cartImage from "../../assets/icons/cart.png"
+</script>
+<div class="h-12 p-2 w-full bg-background border-b-2 border-primary border-opacity-50 flex flex-wrap items-start">
+    <!--<p class="text-red-600 font-bold">This is the navbar. Edit your code here.</p>-->
+    <div class="basis-2/12 flex flex-wrap gap-3 ">
+        <img class="logo-icon" src={logoImage} alt="Logo"/>
+        <p class="align-middle items-baseline">CAFE SANMATHI</p>
+    </div>
+    <div class="basis-9/12 justify-center align-middle content-center" >
+        <div class="flex gap-32 items-center">
+            <p></p>
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Contact</p>
+            <p>Reviews</p>
+        </div>
+    </div>
+    <div class="basis-1/12 justify-end flex flex-wrap gap-4" >
+        <img class="logo-icon" src={cartImage} alt="Cart"/>
+        <p>MENU</p>
+    </div>
 </div>
+
+<style>
+    .logo-icon{
+        height: 38px;
+        width: 38px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
 <script>
 import { Button } from "$lib/components/ui/button";
 </script>
->>>>>>> 435ec7acb7f07aa4b63f3d4ff3e0850cf5796322
