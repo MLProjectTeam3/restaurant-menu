20e8bad545b4d3c9761edcde55c412c32785628b
Vaishnavi397 <nnm22ad065@nmamit.in>
2024-03-23 16:16:47 +0530

    Added Navbar code

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,3 +1,17 @@
-<div class="h-12 p-2 w-full bg-background border-b-2 border-primary border-opacity-15">
-    <p class="text-red-600 font-bold">This is the navbar. Edit your code here.</p>
+<div class="h-12 p-2 w-full bg-background border-b-2 border-primary border-opacity-15 flex justify-center items-center relative">
+    <img src="coffee.svg" alt="Coffee" class="h-8 w-8 absolute left-2 top-2">
+    <span class="absolute left-12 top-1.5 text-4A2C2C font-bold" style="font-size: 1.5rem; font-family:'JetBrains Mono',sans-serif;">CAFE SANMATHI</span>
+
+    <div class="flex justify-between items-center mx-auto" style="width: calc(58% - 15rem);"> <!-- Adjust width as needed -->
+        <a href="#" class="text-4A2C2C font-jockey font-bold" style="font-family: 'JetBrains Mono', sans-serif; font-size: 1.3rem;">Home</a>
+        <a href="#" class="text-4A2C2C font-jockey font-bold ml-4" style="font-family: 'JetBrains Mono', sans-serif; font-size: 1.3rem;">AboutUs</a>
+        <a href="#" class="text-4A2C2C font-jockey font-bold ml-4" style="font-family: 'JetBrains Mono', sans-serif; font-size: 1.3rem;">Contact</a>
+        <a href="#" class="text-4A2C2C font-jockey font-bold ml-4" style="font-family: 'JetBrains Mono',sans-serif; font-size: 1.3rem;">Reviews</a>
+    </div>
+
+    <a href="menu-page.html" class="absolute top-2 right-2 h-8 px-4" style="background-color: transparent; border: 2px solid #FE4C01; color: #FE4C01; font-family: 'JetBrains Mono', monospace; font-weight: bold; font-size:22px; text-decoration: none;">MENU</a>
+
+    <a href="cart-page.html" class="absolute top-2 right-28">
+        <img src="shopping-cart.svg" alt="Shopping Cart" class="h-8 w-8">
+    </a>
 </div>
