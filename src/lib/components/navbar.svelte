9103dcce30d1f6b4945f8716767015596ec8b7de
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-03-25 00:53:30 +0530

    Set up the font style & edited navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,17 +1,28 @@
-<div class="h-12 p-2 w-full bg-background border-b-2 border-primary border-opacity-15 flex justify-center items-center relative">
-    <img src="coffee.svg" alt="Coffee" class="h-8 w-8 absolute left-2 top-2">
-    <span class="absolute left-12 top-1.5 text-4A2C2C font-bold" style="font-size: 1.5rem; font-family:'JetBrains Mono',sans-serif;">CAFE SANMATHI</span>
+<script>
 
-    <div class="flex justify-between items-center mx-auto" style="width: calc(58% - 15rem);"> <!-- Adjust width as needed -->
-        <a href="#" class="text-4A2C2C font-jockey font-bold" style="font-family: 'JetBrains Mono', sans-serif; font-size: 1.3rem;">Home</a>
-        <a href="#" class="text-4A2C2C font-jockey font-bold ml-4" style="font-family: 'JetBrains Mono', sans-serif; font-size: 1.3rem;">AboutUs</a>
-        <a href="#" class="text-4A2C2C font-jockey font-bold ml-4" style="font-family: 'JetBrains Mono', sans-serif; font-size: 1.3rem;">Contact</a>
-        <a href="#" class="text-4A2C2C font-jockey font-bold ml-4" style="font-family: 'JetBrains Mono',sans-serif; font-size: 1.3rem;">Reviews</a>
+  import { Button } from "./ui/button";
+
+</script>
+
+<div class="h-16 px-4 w-full bg-background border-b-2 border-primary border-opacity-15 flex justify-center items-center justify-between fixed top-0">
+
+    <div class="flex justify-center items-center space-x-2">
+        <img src="coffee.svg" alt="Coffee" class="h-8 stroke-primary">
+        <h1 class="text-2xl text-primary">CAFE SANMATHI</h1>
     </div>
 
-    <a href="#" class="absolute top-2 right-2 h-8 px-4" style="background-color: transparent; border: 2px solid #FE4C01; color: #FE4C01; font-family: 'JetBrains Mono', monospace; font-weight: bold; font-size:22px; text-decoration: none;">MENU</a>
+    <div class="flex text-xl font text-primary justify-between items-center space-x-16" > <!-- Adjust width as needed -->
+        <a href="#" >Home</a>
+        <a href="#">AboutUs</a>
+        <a href="#">Contact</a>
+        <a href="#">Reviews</a>
+    </div>
 
-    <a href="#" class="absolute top-2 right-28">
-        <img src="shopping-cart.svg" alt="Shopping Cart" class="h-8 w-8">
-    </a>
+    <div class="flex justify-center items-center space-x-8">
+        <a href="#">
+            <img src="shopping-cart.svg" alt="Shopping Cart" class="h-7">
+        </a>
+        <Button variant="outline">LOGIN</Button>
+    </div>
+    
 </div>
