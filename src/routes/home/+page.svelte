f52d0afdb033937e0a9d5536259bd014c098f4d0
nidhish654 <nidhish.shettigar.06@gmail.com>
2024-04-04 14:01:01 +0530

    Review Section

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import AboutSection from "$lib/components/aboutSection.svelte";
   import ContactSection from "$lib/components/contactSection.svelte";
+  import ReviewSection from "$lib/components/reviewSection.svelte";
   import LandingSection from "$lib/components/landingSection.svelte";
 </script>
 
 <LandingSection />
 <AboutSection />
 <ContactSection />
+<ReviewSection />
