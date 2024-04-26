06f1e1c44df5827c5e52ab6956d54f4cb6878ade
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-04-26 15:32:36 +0530

    feat: search functionality in menu page

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -5,25 +5,6 @@
   import { Switch } from "$lib/components/ui/switch";
 
   let vegOnly = false;
-
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: "dxgkiabgb",
-      apiKey: "599262657695471",
-    },
-  });
-
-  import menu_items from "./menu.json";
-  import { Button } from "$lib/components/ui/button";
-
-  const imageUrls = menu_items.map((item) =>
-    cld
-      .image("MENU/" + item.id.toString()) // Replace with your image's public ID
-      .format("auto")
-      .quality("auto")
-      .toURL()
-  );
-
   const categories = [
     ["Veg Starters", "veg"],
     ["Non Veg Starters", "non-veg"],
@@ -51,20 +32,61 @@
     ["Smoothies", "veg"],
     ["SPL Icecream", "veg"],
   ];
+  let searchTerm = "";
+  let matchingCategories = categories; // State variable to store matching categories
+
+  function buildMatchingCategories() {
+    matchingCategories = [];
+    if (searchTerm === "") {
+      matchingCategories = categories;
+      return;
+    }
+    for(const category of categories){
+      for (const item of menu_items) {
+        if (
+          item.category === category[0] &&
+          item.itemName.toLowerCase().includes(searchTerm.toLowerCase())
+        ){
+          matchingCategories.push(category);
+        break;
+        }
+      }
+    };
+  }
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dxgkiabgb",
+      apiKey: "599262657695471",
+    },
+  });
+
+  import menu_items from "./menu.json";
+  import { Button } from "$lib/components/ui/button";
+
+  const imageUrls = menu_items.map((item) =>
+    cld
+      .image("MENU/" + item.id.toString()) // Replace with your image's public ID
+      .format("auto")
+      .quality("auto")
+      .toURL()
+  );
 </script>
 
-<div class="mt-[11vh] flex flex-col items-center">
+<div class="max-h-[89vh] mt-[11vh] flex flex-col items-center">
   <h1 class="text-4xl text-primary">MENU</h1>
   <input
     type="text"
     placeholder="Search here"
-    class="w-[80%] mb-4 h-10 shadow-lg shadow-zinc-400 rounded-full px-4 text-xl mt-6 bg-background text-primary placeholder-primary border border-b-2 border-primary border-opacity-15"
+    class="w-[80%] mb-4 shadow-lg shadow-zinc-400 rounded-full p-1 px-4 lg:w-[50%] text-xl mt-6 bg-background text-primary placeholder-primary border border-b-2 border-primary border-opacity-15"
+    bind:value={searchTerm}
+    on:input={buildMatchingCategories}
   />
-  <div class="flex mt-4 w-[70%] py-4 justify-between items-center">
+  <div class="flex w-[80%] lg:w-[45%] py-4 justify-between items-center">
     <DropdownMenu.Root>
       <DropdownMenu.Trigger
         class="text-2xl bg-primary text-primary-foreground w-1/2 py-1 rounded-full flex justify-between px-4 items-center"
-        ><p class="font-thin">Categories</p>
+        ><p class="font-thin text-lg">Categories</p>
         <img src="icons/downArrow.png" alt="v" /></DropdownMenu.Trigger
       >
       <DropdownMenu.Content>
@@ -95,40 +117,43 @@
     </div>
   </div>
   <div
-    class="mt-8 flex lg:w-full flex-col items-center max-h-[70vh] overflow-scroll"
+    class="mt-8 flex lg:w-[90%] flex-col items-center max-h-[70vh] overflow-scroll"
+    style="scrollbar-width: none;"
   >
-    {#each categories as category}
-      {#if !vegOnly || (vegOnly && category[1]==="veg")}
-      <h1 class="text-4xl pb-4" id={category[0]}>{category[0]}</h1>
-      <div
-        class="sm:grid sm:grid-cols-2 sm:gap-3 lg:grid-cols-3 sm:w-full sm:px-10"
-      >
-        {#each menu_items as item, index}
-          {#if item.category === category[0]}
-            {#if !vegOnly || (vegOnly && item.type === "Veg")}
-              <Card.Root class="mb-4">
-                <Card.Content
-                  class="w-full h-[120px] flex justify-between border pt-6 items-center"
-                >
-                  <img
-                    src={imageUrls[index]}
-                    alt={item.itemName}
-                    class="h-24 w-24 object-cover rounded-md"
-                  />
-                  <div class="flex flex-col text-2xl">
-                    <h1>{item.itemName}</h1>
-                    <h2>&#8377;{item.price}</h2>
-                  </div>
-                  <Button
-                    class=" bg-[#D9D9D9] px-6 h-16 rounded-md flex justify-center items-center text-2xl text-primary"
-                    >Add</Button
+    {#each matchingCategories as category}
+      {#if !vegOnly || (vegOnly && category[1] === "veg")}
+        <h1 class="text-4xl pb-4" id={category[0]}>{category[0]}</h1>
+        <div
+          class="sm:grid sm:grid-cols-2 sm:gap-3 lg:grid-cols-3 sm:w-full sm:px-10"
+        >
+          {#each menu_items as item, index}
+            {#if item.category === category[0] && item.itemName
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase())}
+              {#if !vegOnly || (vegOnly && item.type === "Veg")}
+                <Card.Root class="mb-4">
+                  <Card.Content
+                    class="w-full h-[120px] flex justify-between border pt-6 items-center"
                   >
-                </Card.Content>
-              </Card.Root>
-          {/if}
-          {/if}
-        {/each}
-      </div>
+                    <img
+                      src={imageUrls[index]}
+                      alt={item.itemName}
+                      class="h-24 w-24 object-cover rounded-md"
+                    />
+                    <div class="flex flex-col text-2xl">
+                      <h1>{item.itemName}</h1>
+                      <h2>&#8377;{item.price}</h2>
+                    </div>
+                    <Button
+                      class=" bg-[#D9D9D9] px-6 h-16 rounded-md flex justify-center items-center text-2xl text-primary"
+                      >Add</Button
+                    >
+                  </Card.Content>
+                </Card.Root>
+              {/if}
+            {/if}
+          {/each}
+        </div>
       {/if}
     {/each}
   </div>
