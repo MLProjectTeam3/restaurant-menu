2a372e2eb9f2c793cb2c09ad295d04899d2136f6
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-04-30 09:34:19 +0530

    feat: logout option and cart position changed in mobile version navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,21 +1,29 @@
 <script>
-// @ts-nocheck
-
+  // @ts-nocheck
   import { Button } from "./ui/button";
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
   import { auth } from "$lib/firebase/firebase.config";
   import { Icon } from "svelte-icons-pack";
   import { FaCircleUser } from "svelte-icons-pack/fa";
   import { LuShoppingCart } from "svelte-icons-pack/lu";
-  import { Separator } from "bits-ui";
-  import { onAuthStateChanged, signOut } from 'firebase/auth'
+  import { onAuthStateChanged, signOut } from "firebase/auth";
   import { FlatToast, ToastContainer, toasts } from "svelte-toasts";
   import { goto } from "$app/navigation";
 
-    let current_user;
-    onAuthStateChanged(auth, (user) => {
-      current_user = user;
-    })
+  let current_user;
+  onAuthStateChanged(auth, (user) => {
+    current_user = user;
+  });
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        toasts.success("Logout Successfull!");
+        goto("/home");
+      })
+      .catch((error) => {
+        toasts.error("Failed to Logout!" + " " + error.message);
+      });
+  };
 </script>
 
 <div
@@ -56,7 +64,11 @@
           <DropdownMenu.Group class="flex flex-col items-center m-4 gap-1">
             <DropdownMenu.Label>My Account</DropdownMenu.Label>
             <DropdownMenu.Separator />
-            <img src="images/landingBg4.jpg" alt="profile" class="w-24 h-24 rounded-md">
+            <img
+              src="images/landingBg4.jpg"
+              alt="profile"
+              class="w-24 h-24 rounded-md"
+            />
             <h1 class="text-xl">
               {current_user.displayName}
             </h1>
@@ -64,17 +76,7 @@
               {current_user.phoneNumber}
             </h1>
             <DropdownMenu.Separator />
-              <Button class="bg-red-600" on:click={()=>{
-                signOut(auth)
-                  .then(()=>{
-                    toasts.success("Logout Successfull!");
-                    goto('/home');
-                  })
-                  .catch((error)=>{
-                    toasts.error("Failed to Logout!"+ " " + error.message);
-                  });
-                  
-              }}>LOGOUT</Button>
+            <Button class="bg-red-600" on:click={handleLogout}>LOGOUT</Button>
           </DropdownMenu.Group>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
@@ -92,37 +94,58 @@
     <h1 class="text-2xl text-primary">CAFE SANMATHI</h1>
   </a>
 
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger>
-      <img src="icons/menu.svg" alt="menu" />
-    </DropdownMenu.Trigger>
-    <DropdownMenu.Content class="w-1/2 bg-background">
-      <DropdownMenu.Group>
-        <DropdownMenu.Item class="text-xl"
-          ><a href="/home">Home</a></DropdownMenu.Item
-        ><DropdownMenu.Separator />
-        <DropdownMenu.Item class="text-xl"
-          ><a href="/menu">Menu</a></DropdownMenu.Item
-        ><DropdownMenu.Separator />
-        <DropdownMenu.Item class="text-xl"
-          ><a href="/home#about">AboutUs</a></DropdownMenu.Item
-        ><DropdownMenu.Separator />
-        <DropdownMenu.Item class="text-xl"
-          ><a href="/home#contact">Contact</a></DropdownMenu.Item
-        ><DropdownMenu.Separator />
-        <DropdownMenu.Item class="text-xl"
-          ><a href="/home#reviews">Reviews</a></DropdownMenu.Item
-        ><DropdownMenu.Separator />
-      </DropdownMenu.Group>
-      <DropdownMenu.Group class="p-2 border-t border-primary border-opacity-25">
-        <div class="flex justify-between items-center">
-          <a href="/login"><Button variant="outline" >LOGIN</Button></a>
-          <a href="/cart"><img src="icons/shoppingCart.svg" alt="menu" /></a>
-        </div>
-      </DropdownMenu.Group>
-    </DropdownMenu.Content>
-  </DropdownMenu.Root>
-  <ToastContainer placement="bottom-right" let:data={data}>
-    <FlatToast {data} /> <!-- Provider template for your toasts -->
+  <div class="flex gap-4">
+    {#if current_user}
+      <a href="/cart"
+        ><Icon src={LuShoppingCart} size="24" className="stroke-primary" /></a
+      >
+    {/if}
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <img src="icons/menu.svg" alt="menu" />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content class="w-1/2 bg-background">
+        <DropdownMenu.Group>
+          <DropdownMenu.Item class="text-xl"
+            ><a href="/home">Home</a></DropdownMenu.Item
+          ><DropdownMenu.Separator />
+          <DropdownMenu.Item class="text-xl"
+            ><a href="/menu">Menu</a></DropdownMenu.Item
+          ><DropdownMenu.Separator />
+          <DropdownMenu.Item class="text-xl"
+            ><a href="/home#about">AboutUs</a></DropdownMenu.Item
+          ><DropdownMenu.Separator />
+          <DropdownMenu.Item class="text-xl"
+            ><a href="/home#contact">Contact</a></DropdownMenu.Item
+          ><DropdownMenu.Separator />
+          <DropdownMenu.Item class="text-xl"
+            ><a href="/home#reviews">Reviews</a></DropdownMenu.Item
+          ><DropdownMenu.Separator />
+        </DropdownMenu.Group>
+        <DropdownMenu.Group
+          class="p-2 border-t border-primary border-opacity-25"
+        >
+          <DropdownMenu.Item class="p-0 items-start data-[highlighted]:bg-transparent flex flex-col gap-2">
+            {#if current_user}
+              <div class="flex gap-2">
+                <img
+                  src="images/landingBg4.jpg"
+                  alt="profile"
+                  class="h-8 w-8 rounded-full"
+                />
+                <h2 class="text-lg">{current_user.displayName}</h2>
+              </div>
+              <Button class="bg-red-600 w-full" on:click={handleLogout}>LOGOUT</Button>
+            {:else}
+              <a href="/login"><Button variant="outline">LOGIN</Button></a>
+            {/if}
+          </DropdownMenu.Item>
+        </DropdownMenu.Group>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </div>
+  <ToastContainer placement="bottom-right" let:data>
+    <FlatToast {data} />
+    <!-- Provider template for your toasts -->
   </ToastContainer>
 </div>
