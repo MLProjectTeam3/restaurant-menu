181aa3b69f1b8712ae5f1f5a9d7347e74c76843e
pranavRBM <pranavbm05@gmail.com>
2024-04-30 22:15:30 +0530

    reading and writing data from database  for navbar,signup,and review pages

--- src/lib/firebase/firebase.config.js
+++ src/lib/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_APIKEY,
@@ -12,7 +13,8 @@ const firebaseConfig = {
 };
 
 // Initializing firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
 // Initializing firebase authentication and getting reference as auth
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
